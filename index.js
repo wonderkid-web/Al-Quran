d00ad2b3afdbb877da5d75f76b0bb4be5ff9ca85
wonderkid-web <mwahyuap2018@gmail.com>
2021-10-01 14:12:21 +0800

    menambahkan fitur baru berupa waktu masuk sholat serta memisahkan file css inline menjadi file css exsternal

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const base_url = "https://api-alquranid.herokuapp.com/surah";
-let ul = document.querySelector('.ul');
+const base_url_quran = "https://api-alquranid.herokuapp.com/surah";
+const base_url_waktu = "https://api.myquran.com/v1/sholat/jadwal/0228/";
+let ul_quran = document.querySelector('.ul_quran');
+let ul_waktu = document.querySelector('.ul_waktu');
+
+waktuSholat();
+function waktuSholat(){
+	let tahun = new Date().getFullYear();
+	let bulan = new Date().getMonth();
+	let tanggal = new Date().getDate();
+	fetch(`${base_url_waktu}/${tahun}/${bulan}/${tanggal}`)
+	.then(res => res.json())
+	.then(res =>{
+		const data = res.data.jadwal;
+		for(const item of Object.entries(data)){
+			let li = `<li class="column">${item[0]} <br>
+					  ${item[1]}</li>
+			`
+			ul_waktu.innerHTML += li;
+		}
+	});
+}
 
 function tampilkanAyat(e){
 	e.preventDefault();
@@ -8,7 +28,7 @@ function tampilkanAyat(e){
 	const form = new FormData(myForm);
 	const query = form.get("cari");
 
-	fetch(`${base_url}/${query}`)
+	fetch(`${base_url_quran}/${query}`)
 	.then(res=>res.json())
 	.then(res=>{
 		const array = res.data;
@@ -21,7 +41,7 @@ function tampilkanAyat(e){
 							<span style="font-size: 20px;"><i>${ayat.tr}</i></span>
 							<hr>
 						  </li>`;
-				ul.innerHTML += li;
+				ul_quran.innerHTML += li;
 			})
 		}
 	});
